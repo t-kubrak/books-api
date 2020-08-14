f975ce8cc7c668f784a2f299d46057d6d7698ea5
t-kubrak <chikashikaari@gmail.com>
2020-08-14 12:50:09 +0300

    add db migrations

---
+++ src/database/migrations/2020_08_14_081513_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->integer('number_of_pages');
+            $table->string('annotation')->nullable();
+            $table->binary('cover_image')->nullable();
+            $table->integer('author_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
