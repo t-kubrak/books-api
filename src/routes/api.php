02f7e01036d22797cd5c59e925fa68933580d3a2
t-kubrak <chikashikaari@gmail.com>
2020-08-14 15:43:03 +0300

    jwt configuration

--- src/routes/api.php
+++ src/routes/api.php
@@ -38,3 +38,12 @@ Route::get('authors/{id}/books', function ($id) {
 
 Route::post('register', 'RegisterController@register');
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
